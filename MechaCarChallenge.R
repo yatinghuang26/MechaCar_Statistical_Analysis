6bd3d6306d81c058e387e6fabc8d53426dd62575
Yating Huang <yatinghuang26@gmail.com>
2022-03-09 15:34:56 -0800

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -32,3 +32,17 @@ lot_summary <- mecha_suspension %>% group_by(Manufacturing_Lot) %>% summarize(Me
                                                                               Std_Dev_PSI=sd(PSI),
                                                                               .groups = 'keep')
 
+#Deliverable 3
+
+#Use t.test() function to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch
+t.test(mecha_suspension$PSI, mu=1500)
+
+#Write three more RScripts using the t.test() function and its subset() argument to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch
+lot1 <- subset(mecha_suspension, Manufacturing_Lot=="Lot1")
+lot2 <- subset(mecha_suspension, Manufacturing_Lot=="Lot2")
+lot3 <- subset(mecha_suspension, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI, mu=1500)
+t.test(lot2$PSI, mu=1500)
+t.test(lot3$PSI, mu=1500)
+
