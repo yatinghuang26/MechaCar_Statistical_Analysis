a7429ad17e5aefc38c206d76bc00857849abfa13
Yating Huang <yatinghuang26@gmail.com>
2022-03-09 15:26:29 -0800

    Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,22 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #Summary Function, Determine p-value and r-squared value for linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg))
 
+#Deliverable 2
+
+#Import and read Suspension_Coil.csv
+mecha_suspension <- read.csv(file="Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
+
+#Create total_summary dataframe
+total_summary <- mecha_suspension %>% summarize(Mean_PSI=mean(PSI),
+                                                Median_PSI=median(PSI),
+                                                Var_PSI=var(PSI),
+                                                Std_Dev_PSI=sd(PSI),
+                                                .groups = 'keep')
+
+#Create lot summary
+lot_summary <- mecha_suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
+                                                                              Median_PSI=median(PSI),
+                                                                              Var_PSI=var(PSI),
+                                                                              Std_Dev_PSI=sd(PSI),
+                                                                              .groups = 'keep')
+
